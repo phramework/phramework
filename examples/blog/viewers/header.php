a5cb74810f871ebf60f60436c612d236cac307cf
Xenophon Spafaridis <Nohponex@gmail.com>
2015-07-09 14:37:56 +0300

    Update examples/blog

--- examples/blog/viewers/header.php
+++ examples/blog/viewers/header.php
@@ -17,15 +17,9 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 
   <style>
-    /*
-     * Style tweaks
-     * --------------------------------------------------
-     */
-
     html,
     body {
       overflow-x: hidden;
-      /* Prevent scroll on narrow devices */
     }
 
     body {
@@ -36,11 +30,6 @@
       padding: 30px 0;
     }
   </style>
-  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-  <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
 </head>
 
 <body>
