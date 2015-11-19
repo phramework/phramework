fd31ce10bbc3e99a1f373f1d1c64b707fea0db24
Xenofon Spafaridis <nohponex@gmail.com>
2015-11-19 20:20:12 +0200

    Add delete model

---
+++ src/Models/SCRUD/Delete.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Copyright 2015 Spafaridis Xenofon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+namespace Phramework\Models\SCRUD;
+
+use Phramework\Models\Database;
+
+/**
+ * Delete model
+ * @license https://www.apache.org/licenses/LICENSE-2.0 Apache-2.0
+ * @author Spafaridis Xenophon <nohponex@gmail.com>
+ * @since 1
+ */
+class Delete
+{
+    public static function delete($id, $additionalAttributes, $table, $index = 'id')
+    {
+        $queryValues = [$id];
+
+        $additional = [];
+
+        foreach ($additionalAttributes as $key => $value) {
+            //push to additional
+            $additional[] = sprintf(
+                'AND "%s"."%s" = ?',
+                $table,
+                $key
+            );
+            $queryValues[] = $value;
+        }
+
+        $query = sprintf(
+            'DELETE FROM "%s"
+            WHERE "%s"."%s" = ?
+              %s
+            LIMIT 1',
+            $table,
+            $table,
+            $index,
+            implode("\n", $additional)
+        );
+
+        return (bool)Database::execute($query, $queryValues);
+    }
+}
