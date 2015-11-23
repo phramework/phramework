887c7780005e129179de03b0a65e17001ad386d0
Xenophon Spafaridis <nohponex@gmail.com>
2015-11-23 19:59:34 +0200

    Update README

--- gh-pages.sh
+++ gh-pages.sh
@@ -8,7 +8,6 @@ mkdir ../phramework-doc
 git clone -b gh-pages git@github.com:phramework/phramework.git ../phramework-doc
 (cd ../phramework-doc && git rm -r .)
 cp -r doc/* ../phramework-doc
-##cd ../phramework-doc
 (cd ../phramework-doc && git add .)
 (cd ../phramework-doc && git commit -a -m 'Update doc')
 (cd ../phramework-doc && git push origin gh-pages)
